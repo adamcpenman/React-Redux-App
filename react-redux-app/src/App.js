001398d1ec850bd2b24ee002a975c92534e678bf
Your Name <you@example.com>
2019-10-24 19:29:44 -0500

    have data, reducer and redux flowing. need to work on styling

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { fetchCharacter } from "./actions/potter";
+import CharacterCard from "./Components/CharacterCard";
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    props.fetchCharacter();
+  }, []);
+
+  if (props.isCharacterLoading) {
+    return <h2>Loading Data...</h2>;
+  }
+
   return (
-    <div className="App">
-    <h1>Redux APP</h1>
+    <div className="card-container">
+      {props.characterError && <p>{props.characterError}</p>}
+      {props.characters.map(character => (
+        <CharacterCard
+          key={character.id}
+          name={character.name}
+          role={character.role}
+          house={character.house}
+        />
+      ))}
     </div>
   );
 }
+function mapStateToProps(state) {
+  return {
+    isCharacterLoading: state.isLoading,
+    characters: state.character,
+    characterError: state.error
+  };
+}
 
-export default App;
+// const mapDispatchToProps = {
+//   fetchCharacter
+// };
+
+export default connect(
+  mapStateToProps,
+  { fetchCharacter }
+)(App);
---
+++ react-redux-app/src/Components/CharacterCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TeamCard = props => {
+  return (
+    <div className="characterCard">
+      <div className="character-list grid-view">
+        <h2 className="characterName"> Name: {props.name} </h2>
+        <h3 className="characterRole"> Role: {props.role} </h3>
+        <p className="characterHouse"> House: {props.house} </p>
+      </div>
+    </div>
+  );
+};
+
+export default TeamCard;
